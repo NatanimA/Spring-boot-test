ccba7c5042d292ea729dffb2934b322e87170a30
NatanimAbesha <Natanimabesha@gmail.com>
2022-07-24 16:10:45 +0300

    ignore it is just a test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@GetMapping
+	public List<Student> hello(){
+		return List.of( new Student(
+				2L,
+				"Natanim",
+				"Natanim@gmail.com",
+				LocalDate.of(2000,2,5 ),
+				21
+		) );
+	}
+
+
 
 }
